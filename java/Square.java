c4acb027267eaf110aaf0ffe3c9814704a897b78
Ri-dwan <abdulrasheedridwan0@gmail.com>
2025-09-12 14:46:58 +0100

    first commit

---
+++ java/Square.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+	public class Square {
+  	public static void main(String [] arge) {
+ 		
+ 	 Scanner input = new Scanner(System.in);
+	System.out.print("Square");
+	int square =3;
+
+	for (;count <=10;){
+	System.out.print("Square");
+
+	count +=3;
+	}
+
+	}
+
+		}
+
+	
